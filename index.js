0c35015102b71fb084ec2ad148b42df949cbe521
ZE0TRON <cmpbilge@gmail.com>
2020-04-15 13:54:35 +0300

    update progress and goal added some bugs fixed

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 const ora = require("ora");
 
-function OraProgressBar(title, steps) {
-  this.steps = steps;
+let isActive = false;
+
+function OraProgressBar(title, goal, options) {
+  if (isActive) throw new Error("Only one spinner can be active at a time");
+  if (goal < 0 || !goal) throw new Error("Invalid goal");
+
+  this.goal = goal;
   this.title = title;
   let currentText = this.title;
   let currentStep = 0;
-  this.spinner = ora(currentText).start();
+  let spinner = {};
 
-  const setText = () => {
-    currentText = `${this.title}  ${currentStep}/${steps}`;
-    this.spinner.text = currentText;
+  if (options) {
+    options.text = currentText;
+    spinner = ora(options).start();
+  } else {
+    spinner = ora(currentText).start();
+  }
+
+  isActive = true;
+
+  const updateText = () => {
+    currentText = `${this.title}  ${currentStep}/${this.goal}`;
+    spinner.text = currentText;
   };
+
+  updateText();
 
   this.progress = (step = 1) => {
     currentStep += step;
-    setText();
-    if (currentStep >= steps) {
-      this.spinner.succeed();
+    updateText();
+    if (currentStep >= this.goal) {
+      this.succeed();
     }
+  };
+
+  this.fail = (text = "") => {
+    spinner.fail(text);
+    isActive = false;
+  };
+
+  this.succeed = (text = "") => {
+    spinner.succeed(text);
+    isActive = false;
+  };
+
+  this.updateProgress = (newProgress) => {
+    if (newProgress > this.goal)
+      throw new Error("Invalid progress, progress cannot be bigger than goal");
+    else if (newProgress === this.goal) {
+      currentStep = newProgress;
+      updateText();
+      this.succeed();
+    }
+
+    currentStep = newProgress;
+    updateText();
+  };
+
+  this.updateGoal = (newGoal) => {
+    if (newGoal < currentStep)
+      throw new Error(
+        "Invalid goal, goal cannot be smaller than current progress"
+      );
+
+    this.goal = newGoal;
+    updateText();
   };
 }
 
